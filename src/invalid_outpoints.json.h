ea016843585ee6e48cf3ea5e8262b2d966483b98
Kees de Wit <keesdewit@keesdewit.nl>
2019-04-01 20:04:45 +0200

    Add checkpoint data

---
+++ src/invalid_outpoints.json.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2018 The Solaris developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef Solaris_INVALID_OUTPOINTS_JSON_H
+#define Solaris_INVALID_OUTPOINTS_JSON_H
+#include <string>
+
+std::string LoadInvalidOutPoints()
+{
+    std::string str = "[]";
+    return str;
+}
+
+#endif //Solaris_INVALID_OUTPOINTS_JSON_H
---
+++ src/invalid_serials.json.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2018 The Solaris developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+
+
+#ifndef Solaris_INVALID_SERIALS_JSON_H
+#define Solaris_INVALID_SERIALS_JSON_H
+
+#include <string>
+
+std::string LoadInvalidSerials()
+{
+    std::string str = "[]";
+    return str;
+}
+
+#endif //Solaris_INVALID_SERIALS_JSON_H
